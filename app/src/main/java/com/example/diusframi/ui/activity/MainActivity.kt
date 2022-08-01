e391dccbad3bb5918cac14fe1b059bfc9d1a3990
Antonio Jesus Ramirez Pachon <ajrpachon@gmail.com>
2022-08-01 20:17:12 +0200

    Implement navigation graph

--- app/src/main/java/com/example/diusframi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/diusframi/ui/activity/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.diusframi.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.diusframi.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diusframi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
